73545d1f2f08c7a78f238247c5441663ced21a16
Andrew T. Baker <abaker@twilio.com>
2015-11-10 15:42:22 -0500

    Adding comments to request validation

--- webhooks/views.py
+++ webhooks/views.py
@@ -12,13 +12,18 @@ def validate_twilio_request(f):
     """Validates that incoming requests genuinely originated from Twilio"""
     @wraps(f)
     def decorated_function(request, *args, **kwargs):
+        # Create an instance of the RequestValidator class
         validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
 
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
         request_valid = validator.validate(
             request.build_absolute_uri(),
             request.POST,
             request.META['HTTP_X_TWILIO_SIGNATURE'])
 
+        # Continue processing the request if it's valid, return a 403 error if
+        # it's not
         if request_valid:
             return f(request, *args, **kwargs)
         else:
